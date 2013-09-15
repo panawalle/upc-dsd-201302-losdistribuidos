106d749629f09fe7dd11fb397964366ed8630fbc
lcermenon <lcermenon@gmail.com>
2013-09-15 16:17:38 +0000

    Se agregan las clases: Cliente.cs Color.cs Modelo.cs Vehiculo.cs
    Atte.
    Luchito Cabañas

---
+++ ReservasWeb/ReservasWeb/Dominio/Modelo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReservasWeb.Dominio
+{
+    public class Modelo
+    {
+    }
+}
---
+++ ReservasWeb/ReservasWeb/Dominio/Vehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReservasWeb.Dominio
+{
+    public class Vehiculo
+    {
+        public string placa { get; set; }
+        public string vin { get; set; }
+        public Color color { get; set; }
+        public Modelo modelo { get; set; }
+        public string anio { get; set; }
+        public string motor { get; set; }
+        public string contacto { get; set; }
+        public string usuario { get; set; }
+        public string fecha { get; set; }
+        public Cliente cliente { get; set; }
+    }
+}
