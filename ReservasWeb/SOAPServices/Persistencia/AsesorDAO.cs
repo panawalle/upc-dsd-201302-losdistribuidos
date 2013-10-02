d27319f6406c626c57dba9985883d94214671c44
luis.cabanasv <luis.cabanasv@f841c34f-9dbc-8ac4-55f6-b431db31b501>
2013-10-02 00:26:42 +0000

    Se agregan los daos para el registro de reserva

---
+++ ReservasWeb/SOAPServices/Persistencia/AsesorDAO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using SOAPService.Dominio;
+
+namespace SOAPService.Persistencia
+{
+    public class AsesorDAO
+    {
+
+        ConexionUtil objConUtil = new ConexionUtil();
+
+        public Dominio.Asesor fnObtenerAsesor(int numCodigoAsesor)
+        {
+
+            SqlConnection objSqlCon = new SqlConnection();
+            objSqlCon = objConUtil.fnObtenerConexion();
+            objSqlCon.Open();
+            Dominio.Asesor objAsesor = new Dominio.Asesor();
+
+            try
+            {
+                DataSet ds = new DataSet();
+                DataTable dtAsesor = new DataTable();
+
+                SqlDataAdapter cmd = new SqlDataAdapter("sp_buscarAsesor", objSqlCon);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.SelectCommand.Parameters.Add("@numCodigoAsesor", SqlDbType.Int).Value = numCodigoAsesor;
+                cmd.SelectCommand.ExecuteNonQuery();
+                cmd.Fill(ds);
+                dtAsesor  = ds.Tables[0];
+
+                if (dtAsesor.Rows.Count > 0)
+                {
+                    foreach (DataRow dr in dtAsesor.Rows)
+                    {
+                        objAsesor.numCodigoAsesor = Convert.ToInt32(dr["numCodigoAsesor"]);
+                        objAsesor.nombre = dr["nombre"].ToString();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                if (objSqlCon.State == ConnectionState.Open)
+                {
+                    objSqlCon.Close();
+                }
+                throw;
+            }
+            finally
+            {
+
+                if (objSqlCon.State == ConnectionState.Open)
+                {
+                    objSqlCon.Close();
+                }
+            }
+            return objAsesor;
+        }
+
+    }
+}
